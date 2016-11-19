2309b8e8110cd08821df6265fd640bbe02319a1a
Jyoti343 <jyotijenkin@gmail.com>
2016-11-19 14:31:23 +0530

    c12 changed firefox to a simple print java code to run in jenkins for the very first time

--- dele1/Delay1.java
+++ dele1/Delay1.java
@@ -9,12 +9,5 @@ public class Delay1 {
 
 	public static void main(String sd[]){
 		
-		File fuck=new File("C:\\Users\\JYOTI\\Downloads\\geckodriver-v0.11.1-win32\\geckodriver.exe");
-		//System.setProperty("webdriver.gecko.driver", fuck.getAbsolutePath());
-		System.out.println("i's not u thought i was,,"+" *** "+fuck.getAbsolutePath());
-		WebDriver driver=new FirefoxDriver();
-		driver.navigate().to("http://google.co.in");
-		System.out.println(driver.getPageSource());
-		
-	}
+       System.out.println("Jyoti loves sunny and maze also");
 }
